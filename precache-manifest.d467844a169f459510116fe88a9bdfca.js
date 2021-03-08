14eedd59cf55922aab3bd69c6042ec7d90039125
Nikolakaric85 <nikolakaricsd@gmail.com>
2021-03-08 18:11:45 +0100

    Updates

---
+++ precache-manifest.d467844a169f459510116fe88a9bdfca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07d8b699a90f15aa96246764c130533f",
+    "url": "/IdeaNotes/index.html"
+  },
+  {
+    "revision": "866839969a7b421d72ed",
+    "url": "/IdeaNotes/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "cfb87070a7e95fbdc816",
+    "url": "/IdeaNotes/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "866839969a7b421d72ed",
+    "url": "/IdeaNotes/static/js/2.dcf048de.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/IdeaNotes/static/js/2.dcf048de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cfb87070a7e95fbdc816",
+    "url": "/IdeaNotes/static/js/main.30a43076.chunk.js"
+  },
+  {
+    "revision": "dcb6a676c45221223b7f",
+    "url": "/IdeaNotes/static/js/runtime-main.623b92ac.js"
+  }
+]);
